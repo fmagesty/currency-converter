e0a6cc2a29728d0fcc99d3b189580874b7d7e43e
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-02-16 13:35:38 -0300

    Add files via upload

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,8 +1,11 @@
-// Function that converts BRL to DOLLAR
+// Function that converts BRL to DOL
 function convDol(n) {
+    n = document.getElementById('brl-input').value;
     n /= 5.37;
+    // n = Math.round(n); // if needed to round the value
+    document.getElementById('result').textContent = 'Value in dollar: $' + n;
     return n;
 }
 
-console.log(convDol(100))
+
 
