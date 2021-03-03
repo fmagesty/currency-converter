2e07f4809ad0106dc185006e8c080b217ca7b7f5
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-03-03 11:18:11 -0300

    Add files via upload

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,11 +1,35 @@
-// Function that converts BRL to DOL
-function convDol(n) {
-    n = document.getElementById('brl-input').value;
-    n /= 5.37;
-    n = n.toFixed(2); // round the value with 2 decimals
-    document.getElementById('result').textContent = 'Value in dollar: $' + n; //displays the result
-    return n;
+// Function that converts CURRENCY
+const convCur = async (curr) => {
+    const userInput = document.getElementById('val-input').value;
+
+    const ratio = async () => {
+        const response = await fetch('https://api.exchangeratesapi.io/latest')
+        const responseBody = await response.json();
+        return responseBody.rates[curr];
+    }
+    
+    const ratioResult = await ratio();
+    const result = (userInput/ratioResult);
+    
+    document.getElementById('result').textContent = `Value in dollar: ${result.toFixed(2)}`;
 }
 
 
 
+
+
+
+// // Function that converts BRL to DOL
+// const convDol = async () => {
+//     const userInput = document.getElementById('brl-input').value;
+    
+//     const ratio = async () => {
+//         const response = await fetch('https://api.exchangeratesapi.io/latest?base=USD')
+//         const responseBody = await response.json();
+//         return responseBody.rates['BRL'];
+//     }
+//     const ratioResult = await ratio();
+//     const result = (userInput/ratioResult);
+    
+//     document.getElementById('result').textContent = `Value in dollar: ${result.toFixed(2)}`  ;
+// }
