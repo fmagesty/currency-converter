115349b7fa583e3419ae390b0f81d6e652dae044
Felipe Melgaço Magesty Silveira <fmagesty@gmail.com>
2021-03-03 14:16:16 -0300

    Add files via upload

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,35 +1,52 @@
+// Moeda de entrada
+const clickDropdown1 = (dropButton1) => {
+    // Troca o nome 'Moeda de entrada' pela moeda
+    document.getElementById('entrada').innerHTML = document.getElementById(dropButton1).innerHTML;
+    // Cria variavel pra moeda de entrada
+    const entrada = document.getElementById(dropButton1).innerHTML;
+}
+// Moeda de saida
+const clickDropdown2 = (dropButton2) => {
+    // Troca o nome 'Moeda de entrada' pela moeda
+    document.getElementById('saida').innerHTML = document.getElementById(dropButton2).innerHTML;
+    // Cria variavel pra moeda de entrada
+    const saida = document.getElementById(dropButton2).innerHTML;
+}
+
 // Function that converts CURRENCY
 const convCur = async (curr) => {
-    const userInput = document.getElementById('val-input').value;
+    const inputVal = document.getElementById('val-input').value;
 
-    const ratio = async () => {
-        const response = await fetch('https://api.exchangeratesapi.io/latest')
+    const toRatio = async (curr2) => {
+        const response = await fetch(`https://api.exchangeratesapi.io/latest?base=${saida.innerHTML}`)
+        console.log(response); //////
         const responseBody = await response.json();
         return responseBody.rates[curr];
     }
     
-    const ratioResult = await ratio();
-    const result = (userInput/ratioResult);
-    
-    document.getElementById('result').textContent = `Value in dollar: ${result.toFixed(2)}`;
+    const ratioResult = await toRatio();
+    const result = (inputVal/ratioResult);
+    document.getElementById('result').textContent = `$${result.toFixed(2)}`;
 }
 
 
 
+/// KEEPING THIS JUST IN CASE XD
+// // Function that converts CURRENCY
+// const convCur = async (curr) => {
+//     const inputVal = document.getElementById('val-input').value;
 
-
-
-// // Function that converts BRL to DOL
-// const convDol = async () => {
-//     const userInput = document.getElementById('brl-input').value;
-    
-//     const ratio = async () => {
-//         const response = await fetch('https://api.exchangeratesapi.io/latest?base=USD')
+//     const toRatio = async () => {
+//         const response = await fetch('https://api.exchangeratesapi.io/latest')
 //         const responseBody = await response.json();
-//         return responseBody.rates['BRL'];
+//         return responseBody.rates[curr];
 //     }
-//     const ratioResult = await ratio();
-//     const result = (userInput/ratioResult);
     
-//     document.getElementById('result').textContent = `Value in dollar: ${result.toFixed(2)}`  ;
+//     const ratioResult = await toRatio();
+//     const result = (inputVal/ratioResult);
+//     document.getElementById('result').textContent = `$${result.toFixed(2)}`;
 // }
+
+
+
+
